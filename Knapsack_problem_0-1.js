64b9983efc5eecfc72cc35ebf46df20439f0e9c9
Anton <164165753+AkoHb@users.noreply.github.com>
2024-10-27 21:01:51 +0200

    Update Knapsack_problem_0-1.js

--- Knapsack_problem_0-1.js
+++ Knapsack_problem_0-1.js
@@ -83,34 +83,34 @@ const test = () => {
     const stTime = performance.now();
     console.log("Test is begin...")
     const validAns = [
-        // [
-        //     [
-        //         { name:'map', weight:9, value:150 }, 
-        //         { name:'compass', weight:13, value:35 }, 
-        //         { name:'water', weight:153, value:200 }, 
-        //         { name:'sandwich', weight:50, value:160 },
-        //         { name:'glucose', weight:15, value:60 }, 
-        //         { name:'tin', weight:68, value:45 }, 
-        //         { name:'banana', weight:27, value:60 }, 
-        //         { name:'apple', weight:39, value:40 }
-        //     ], 
-        //     100, 
-        //     405
-        // ],
-        // [
-        //     [
-        //         { name:'map', weight:9, value:150 }, 
-        //         { name:'compass', weight:13, value:35 }, 
-        //         { name:'water', weight:153, value:200 }, 
-        //         { name:'sandwich', weight:50, value:160 }, 
-        //         { name:'glucose', weight:15, value:60 }, 
-        //         { name:'tin', weight:68, value:45 }, 
-        //         { name:'banana', weight:27, value:60 }, 
-        //         { name:'apple', weight:39, value:40 }
-        //     ], 
-        //     200,
-        //     510
-        // ],
+        [
+            [
+                { name:'map', weight:9, value:150 }, 
+                { name:'compass', weight:13, value:35 }, 
+                { name:'water', weight:153, value:200 }, 
+                { name:'sandwich', weight:50, value:160 },
+                { name:'glucose', weight:15, value:60 }, 
+                { name:'tin', weight:68, value:45 }, 
+                { name:'banana', weight:27, value:60 }, 
+                { name:'apple', weight:39, value:40 }
+            ], 
+            100, 
+            405
+        ],
+        [
+            [
+                { name:'map', weight:9, value:150 }, 
+                { name:'compass', weight:13, value:35 }, 
+                { name:'water', weight:153, value:200 }, 
+                { name:'sandwich', weight:50, value:160 }, 
+                { name:'glucose', weight:15, value:60 }, 
+                { name:'tin', weight:68, value:45 }, 
+                { name:'banana', weight:27, value:60 }, 
+                { name:'apple', weight:39, value:40 }
+            ], 
+            200,
+            510
+        ],
         [
             [
                 { name:'cheese', weight:23, value:30 }, 
@@ -124,45 +124,45 @@ const test = () => {
             100,
             145
         ],
-        // [
-        //     [
-        //         { name:'cheese', weight:23, value:30 }, 
-        //         { name:'beer', weight:52, value:10 }, 
-        //         { name:'suntan cream', weight:11, value:70 }, 
-        //         { name:'camera', weight:32, value:30 }, 
-        //         { name:'T-shirt', weight:24, value:15 }, 
-        //         { name:'trousers', weight:48, value:10 }, 
-        //         { name:'umbrella', weight:73, value:40 }
-        //     ], 
-        //     200,
-        //     185
-        // ],
-        // [
-        //     [
-        //         { name:'waterproof trousers', weight:42, value:70 }, 
-        //         { name:'waterproof overclothes', weight:43, value:75 }, 
-        //         { name:'note-case', weight:22, value:80 }, 
-        //         { name:'sunglasses', weight:7, value:20 }, 
-        //         { name:'towel', weight:18, value:12 }, 
-        //         { name:'socks', weight:4, value:50 }, 
-        //         { name:'book', weight:30, value:10 }
-        //     ], 
-        //     100,
-        //     237
-        // ],
-        // [
-        //     [
-        //         { name:'waterproof trousers', weight:42, value:70 }, 
-        //         { name:'waterproof overclothes', weight:43, value:75 }, 
-        //         { name:'note-case', weight:22, value:80 }, 
-        //         { name:'sunglasses', weight:7, value:20 }, 
-        //         { name:'towel', weight:18, value:12 }, 
-        //         { name:'socks', weight:4, value:50 }, 
-        //         { name:'book', weight:30, value:10 }
-        //     ], 
-        //     200,
-        //     317
-        // ]
+        [
+            [
+                { name:'cheese', weight:23, value:30 }, 
+                { name:'beer', weight:52, value:10 }, 
+                { name:'suntan cream', weight:11, value:70 }, 
+                { name:'camera', weight:32, value:30 }, 
+                { name:'T-shirt', weight:24, value:15 }, 
+                { name:'trousers', weight:48, value:10 }, 
+                { name:'umbrella', weight:73, value:40 }
+            ], 
+            200,
+            185
+        ],
+        [
+            [
+                { name:'waterproof trousers', weight:42, value:70 }, 
+                { name:'waterproof overclothes', weight:43, value:75 }, 
+                { name:'note-case', weight:22, value:80 }, 
+                { name:'sunglasses', weight:7, value:20 }, 
+                { name:'towel', weight:18, value:12 }, 
+                { name:'socks', weight:4, value:50 }, 
+                { name:'book', weight:30, value:10 }
+            ], 
+            100,
+            237
+        ],
+        [
+            [
+                { name:'waterproof trousers', weight:42, value:70 }, 
+                { name:'waterproof overclothes', weight:43, value:75 }, 
+                { name:'note-case', weight:22, value:80 }, 
+                { name:'sunglasses', weight:7, value:20 }, 
+                { name:'towel', weight:18, value:12 }, 
+                { name:'socks', weight:4, value:50 }, 
+                { name:'book', weight:30, value:10 }
+            ], 
+            200,
+            317
+        ]
     ];
 
     let msg = [[], []]; // [[true], [false]]
